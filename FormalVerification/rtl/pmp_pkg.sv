d2d7a32a72031164b5bfc03fccc112aa9e39783b
TheSaltyLiquorice <60432441+TheSaltyLiquorice@users.noreply.github.com>
2022-05-12 10:55:02 +0200

    Create pmp_pkg.sv

---
+++ FormalVerification/rtl/pmp_pkg.sv
@@ -0,0 +1,14 @@
+package pmp_pkg;
+	`ifndef pmpaddrbits
+	`define pmpaddrbits 54
+	`endif
+
+	`ifndef pmp_entries
+	`define pmp_entries 16
+	`endif
+
+	typedef reg[`pmpaddrbits:0] pmpaddr_type;
+	typedef pmpaddr_type [0:`pmp_entries-1] pmpaddr_vec_type;  
+	typedef reg[63:0] word64;
+
+endpackage
